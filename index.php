73898e45dfccb4c0fd4ce5172198fb8bed7cc2f5
github123 <123github>
2022-03-26 00:53:50 +0600

    variable

---
+++ index.php
@@ -0,0 +1,24 @@
+<!-- Variable -->
+<!-- Common Variable -->
+<?php
+$name = "mahamudul";
+echo $name;
+?>
+
+<?php
+$book="bangla";
+$book="English";
+$book="math";
+echo $book;
+?>
+
+<!-- constant variable -->
+<?php
+define('path','localhost/phpclass_2');
+echo path;
+?>
+
+<?php
+define('my_name','karim',true);
+echo My_name;
+?>
